fe5afc8698025822d4c999c1ab1b9140800119c9
emmachlo <nassim.morouche1@gmail.com>
2019-03-13 16:07:05 +0100

    correction erreur ville inconnu

--- routes/ville.js
+++ routes/ville.js
@@ -3,10 +3,25 @@ var router = express.Router();
 var request = require("request");
 
 /* GET users listing. */
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
   request(`https://geocode.xyz/${req.body.nom_ville}?json=1&auth=125570002384179154319x1962`, function(error, response, body) {
     let jsonParse = JSON.parse(body);
-    res.render('ville', { nom_ville: req.body.nom_ville, description: req.body.description, latitude: jsonParse.latt, longitude: jsonParse.longt});
+      if(jsonParse.error == null || jsonParse.error == undefined){
+        res.render('ville', {
+          nom_ville: req.body.nom_ville,
+          description: req.body.description,
+          latitude: jsonParse.latt,
+          longitude: jsonParse.longt,
+          display: 'block'
+        });
+      }
+      else {
+        console.log('ERROR');
+        res.render('ville', {
+          nom_ville: 'La ville entrée est inconnue',
+          display: 'none'
+        });
+      }
   });
 });
 
