2866a98f7f666ba41ff2028bcbf1bd76f64f1397
emmachlo <nassim.morouche1@gmail.com>
2019-03-13 12:12:13 +0100

    POST nom_ville

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
 
+router.post('/ville', function(req, res) {
+  res.render('ville', { nom_ville: req.body.nom_ville });
+});
+
 module.exports = router;
