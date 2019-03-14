5485cd4d0ca7cb7c4a548bef56ade509fa68f4f0
emmachlo <nassim.morouche1@gmail.com>
2019-03-14 15:29:43 +0100

    Test bonus error display heroku

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
 import test from 'ava';
 var axios = require('axios');
-/*
-test('test form', async t => {
-  await request('https://evening-eyrie-83553.herokuapp.com/', (error, response, body) => {
-    console.log(body);
-    t.pass();
-  });
-});
-*/
 
 test('Test form exist', async t => {
-  const res = await axios.get('http://localhost:3000/');
+  const res = await axios.get('https://evening-eyrie-83553.herokuapp.com/');
   let test = res.data.toString().includes('<form');
   t.is(test, true);
 });
@@ -18,10 +10,24 @@ test('Test form exist', async t => {
 test('Test ville identique', async t => {
   try {
     let ville = 'Villepinte'
-    const res = await axios.post('http://localhost:3000/ville', {
+    const res = await axios.post('https://evening-eyrie-83553.herokuapp.com/ville', {
       nom_ville: ville
     });
     var test = res.data.toString().includes(ville);
+  }
+  catch (error) {
+    console.log('error');
+  }
+  t.is(test, true);
+})
+
+test('Test ville error', async t => {
+  try {
+    let ville = 'zefkjhzefkjnzef'
+    const res = await axios.post('https://evening-eyrie-83553.herokuapp.com/ville', {
+      nom_ville: ville
+    });
+    var test = res.data.toString().includes('La ville entrée est inconnue');
   }
   catch (error) {
     console.log('error');
