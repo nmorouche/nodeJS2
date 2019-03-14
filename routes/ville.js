01d7cfd1e6bf7f6e001fb6418bd0c51420bb4d57
emmachlo <nassim.morouche1@gmail.com>
2019-03-14 22:23:13 +0100

    test integration + fonctionnel nock

--- routes/ville.js
+++ routes/ville.js
@@ -1,28 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var request = require("request");
+var request = require('request');
+var axios = require('axios');
+var lacite;
 
-/* GET users listing. */
-router.post('/', function(req, res) {
-  request(`https://geocode.xyz/${req.body.nom_ville}?json=1&auth=125570002384179154319x1962`, async function(error, response, body) {
-    let jsonParse = JSON.parse(body);
-      if(jsonParse.error == null || jsonParse.error == undefined){
-        res.render('ville', {
-          nom_ville: req.body.nom_ville,
-          description: req.body.description,
-          latitude: jsonParse.latt,
-          longitude: jsonParse.longt,
-          display: 'block'
-        });
-      }
-      else {
-        console.log('ERROR');
-        res.render('ville', {
-          nom_ville: 'La ville entrée est inconnue',
-          display: 'none'
-        });
-      }
-  });
+router.post('/', async function(req, res) {
+  try {
+    lacite = req.body.nom_ville;
+    const response = await axios.get(`https://geocode.xyz/${req.body.nom_ville}?json=1&auth=759315464425069903834x1979`);
+    console.log(response.data);
+    if(response.data.error == null || response.data.error == undefined){
+      res.render('ville', {
+        nom_ville: req.body.nom_ville,
+        description: req.body.description,
+        latitude: response.data.latt,
+        longitude: response.data.longt,
+        display: 'block'
+      });
+    }
+    else {
+      console.log('ERROR');
+      res.render('ville', {
+        nom_ville: 'La ville entrée est inconnue',
+        display: 'none'
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.render('ville', {
+      nom_ville: 'Geocode is down',
+      display: 'none'
+    });
+  }
 });
 
 module.exports = router;
--- test.js
+++ test.js
@@ -1,36 +1,99 @@
 import test from 'ava';
 var axios = require('axios');
+const nock = require('nock');
+const port = 3000;
+const app = require('./app.js');
 
-test('Test form exist', async t => {
-  const res = await axios.get('https://evening-eyrie-83553.herokuapp.com/');
-  let test = res.data.toString().includes('<form');
-  t.is(test, true);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cityRouter = require('./routes/ville');
+app.use('/index', indexRouter);
+app.use('/users', usersRouter);
+app.use('/ville', cityRouter);
+
+app.listen(port, () => {
+
+  test('Test form exist', async t => {
+    const res = await axios.get('http://localhost:3000/');
+    let test = res.data.toString().includes('<form');
+    t.is(test, true);
+  });
+
+  test('Test ville identique', async t => {
+    try {
+      const city = 'berlin';
+      const longt = 26;
+      const latt = 22;
+      nock('https://geocode.xyz')
+      .get(`/${city}?json=1&auth=759315464425069903834x1979`)
+      .reply(200, { longt: longt, latt: latt});
+
+      const res = await axios.post('http://localhost:3000/ville', {
+        nom_ville: city
+      });
+      var test = res.data.toString().includes(city);
+    }
+    catch (error) {
+      console.log(error);
+    }
+    t.is(test, true);
+  });
+
+  test('Test ville error', async t => {
+    try {
+      const city = 'zefkjhzefkjnzef';
+      const longt = 26;
+      const latt = 22;
+      nock('https://geocode.xyz')
+      .get(`/${city}?json=1&auth=759315464425069903834x1979`)
+      .reply(200, { error:1});
+
+      const res = await axios.post('http://localhost:3000/ville', {
+        nom_ville: city
+      });
+      var test = res.data.toString().includes('La ville entrée est inconnue');
+    }
+    catch (error) {
+      console.log(error);
+    }
+    t.is(test, true);
+  });
+
+  test('test geocode 404', async t => {
+    try {
+      const city = 'zefkjhzefkjnzef';
+      const longt = 26;
+      const latt = 22;
+      nock('https://geocode.xyz')
+      .get(`/${city}?json=1&auth=759315464425069903834x1979`)
+      .reply(404, { error:1 });
+
+      const res = await axios.post('http://localhost:3000/ville', {
+        nom_ville: city
+      });
+      var test = res.data.toString().includes('Geocode is down');
+    }
+    catch (error) {
+      console.log(error);
+    }
+    t.is(test, true);
+  });
+
+  test('Test ville nock', async t => {
+    try {
+      const city = 'berlin';
+      const longt = 26;
+      const latt = 22;
+      nock('https://geocode.xyz')
+      .get(`/${city}?json=1&auth=759315464425069903834x1979`)
+      .reply(200, { longt: longt, latt: latt});
+
+      const { data } = await axios.post('http://localhost:3000/ville', { nom_ville: city });
+      t.true(data.toString().includes(`https://maps.google.com/maps?q=${latt},${longt}&hl=es;z=14&amp;output=embed`))
+    }
+    catch (error) {
+      console.log('error');
+      t.fail();
+    }
+  });
 });
-
-test('Test ville identique', async t => {
-  try {
-    let ville = 'Villepinte'
-    const res = await axios.post('https://evening-eyrie-83553.herokuapp.com/ville', {
-      nom_ville: ville
-    });
-    var test = res.data.toString().includes(ville);
-  }
-  catch (error) {
-    console.log('error');
-  }
-  t.is(test, true);
-})
-
-test('Test ville error', async t => {
-  try {
-    let ville = 'zefkjhzefkjnzef'
-    const res = await axios.post('https://evening-eyrie-83553.herokuapp.com/ville', {
-      nom_ville: ville
-    });
-    var test = res.data.toString().includes('La ville entrée est inconnue');
-  }
-  catch (error) {
-    console.log('error');
-  }
-  t.is(test, true);
-})
