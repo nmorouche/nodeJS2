1cb93902724086629c5bfa54a3717e28b7b20afc
emmachlo <nassim.morouche1@gmail.com>
2019-03-14 15:10:02 +0100

    test post prod

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import axios from 'axios'
+var axios = require('axios');
 /*
 test('test form', async t => {
   await request('https://evening-eyrie-83553.herokuapp.com/', (error, response, body) => {
@@ -8,9 +8,23 @@ test('test form', async t => {
   });
 });
 */
-test('Test requete', async t => {
+
+test('Test form exist', async t => {
   const res = await axios.get('https://evening-eyrie-83553.herokuapp.com');
   let test = res.data.toString().includes('<form');
   t.is(test, true);
-
 });
+
+test('Test ville identique', async t => {
+  try {
+    let ville = 'Villepinte'
+    const res = await axios.post('https://evening-eyrie-83553.herokuapp.com/ville', {
+      nom_ville: ville
+    });
+    var test = res.data.toString().includes(ville);
+  }
+  catch (error) {
+    console.log('error');
+  }
+  t.is(test, true);
+})
